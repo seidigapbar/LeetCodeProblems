711241514ee34238c27b22fa36a18c3d5215abdc
sseidigapbar <seidigapbarsanzhar@gmail.com>
2023-01-04 17:50:50 +0100

    Solved the problem much more beatifully

--- LinkedList/DeleteNodeInALinkedList.cpp
+++ LinkedList/DeleteNodeInALinkedList.cpp
@@ -1,3 +1,22 @@
+# Much better two liner: 
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
