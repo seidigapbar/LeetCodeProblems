6e05fba186baa492412bceac0c9c3eeb7a3a0f58
seidigapbar <seidigapbarsanzhar@gmail.com>
2023-01-14 13:06:33 +0100

    Solved via XOR and counting 1 bits

---
+++ Others/HammingDistance.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z = x ^ y, cnt = 0;
+        while(z != 0){
+            z = z & (z - 1);
+            cnt++;
+        }
+        return cnt;
+    }
+};
