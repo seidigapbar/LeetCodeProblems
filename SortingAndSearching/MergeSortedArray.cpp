ebcfe0003aa63e97ec8f2e3f12e58eb7db114747
seidigapbar <seidigapbarsanzhar@gmail.com>
2023-01-22 18:51:35 +0100

    TODO: Revise

---
+++ SortingAndSearching/MergeSortedArray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1;
+        
+        while(i > -1 && j > -1){
+            if(nums1[i] > nums2[j]){
+                nums1[i + j + 1] = nums1[i];
+                i--;
+            } else{
+                nums1[i + j + 1] = nums2[j];
+                j--;
+            }
+        }
+        
+        while(j > -1){
+            nums1[j] = nums2[j];
+            j--;
+        }
+    }
+};
